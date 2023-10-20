595b64555060544cfcb0aa547e2441b78fe8f6f3
Luciano Reguera <regueraluciano@gmail.com>
2023-10-19 22:37:30 -0300

    can fetch all articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ArticleCollection;
 use App\Http\Resources\ArticleResource;
 use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    public function index(): ArticleCollection
+    {
+        $articles = Article::all();
+
+        return ArticleCollection::make($articles);
+    }
+
     public function show(Article $article): ArticleResource
     {
         return ArticleResource::make($article);
